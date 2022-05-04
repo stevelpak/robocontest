b285c5c57e5b01200f11cdb0d928b95d58edcad5
stevelpak <stevelpak@gmail.com>
2022-05-04 17:09:03 +0500

    Second Problem Solution

--- 002.dart
+++ 002.dart
@@ -1,1 +1,19 @@
+import 'dart:io';
 
+main() async {
+  Iterable<int> nums =
+      stdin.readLineSync()!.split(" ").map((e) => int.parse(e));
+
+  int num1 = nums.elementAt(0);
+  int num2 = nums.elementAt(1);
+
+  if (num1 > num2) {
+    print(">");
+  } else if (num2 > num1) {
+    print("<");
+  } else {
+    print("=");
+  }
+
+  exit(0);
+}
