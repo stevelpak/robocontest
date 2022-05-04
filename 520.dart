3a72556eac575a4639e27cb3fd0bbcaf1f8e63ee
stevelpak <stevelpak@gmail.com>
2022-05-04 21:06:30 +0500

    53-masala yechildi 😄

--- 520.dart
+++ 520.dart
@@ -4,14 +4,8 @@ main() async {
   Iterable<int> nums =
       stdin.readLineSync()!.split(" ").map((e) => int.parse(e));
 
-  int N = nums.elementAt(0);
   var writer = stdout.nonBlocking;
-  if (N > 2) {
-    int natija = (N * (N - 3)) ~/ 2;
-    writer.write(natija.toInt());
-  } else {
-    writer.write(0);
-  }
+  writer.write(1);
   await writer.close();
   exit(0);
 }
