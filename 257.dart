2cd57776aad291b50433abd53d5a29d12acffba3
stevelpak <stevelpak@gmail.com>
2022-05-07 08:36:11 +0500

    257-yechim Time Limit olmoqda

--- 257.dart
+++ 257.dart
@@ -1,10 +1,22 @@
 import 'dart:io';
 
 main() async {
-  Iterable<int> nums =
-      stdin.readLineSync()!.split(" ").map((e) => int.parse(e));
+  String num = "";
+  int c = 0;
+  num = "0" + stdin.readLineSync()!.toString();
+
+  for (var i = 0; i < num.length - 1; i++) {
+    if (num[i] == "0" && num[i + 1] == "1" && c <= 2) {
+      c += 1;
+    }
+  }
 
   var writer = stdout.nonBlocking;
+  if (c <= 1) {
+    writer.write("YES");
+  } else {
+    writer.write("NO");
+  }
   await writer.close();
   exit(0);
 }
